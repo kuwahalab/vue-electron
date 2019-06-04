743dbe3f88ea8d6e1b595dea5265828ce47682fb
kuwahalab <kuwahara.shohei@slogan.jp>
2019-06-04 23:02:52 +0900

    電卓のボタンを配置した。

---
+++ src/components/Calc.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div
+      v-for="(value, index) in buttons"
+      :key="index"
+    >
+      <button
+        v-for="item in value"
+        :key="item"
+      >
+        {{ item }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      buttons: [
+        ["AC", "+/-", "%", "/"],
+        [7, 8, 9, "*"],
+        [4, 5, 6, "-"],
+        [1, 2, 3, "+"],
+        [0, ".", "="]
+      ]
+    }
+  }
+}
+</script>
